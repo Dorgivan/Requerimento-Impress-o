06d931925f1946b54447c861716ae00df64c92ff
Dorgivan <39264164+Dorgivan@users.noreply.github.com>
2018-07-10 14:00:18 -0300

    Update models.py

--- requerimento_impressao/app/models.py
+++ requerimento_impressao/app/models.py
@@ -1,18 +1,8 @@
 from django.db import models
 import uuid
 
-class Arquivo(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    descricao = models.CharField(max_length = 255, blank = False)
-    data = models.DateField(auto_now_add = True)
-    copias = models.IntegerField(blank = False, null = False)
-    nome = models.CharField(max_length = 70, blank = False)
-
-    def __str__(self):
-        return self.nome
-
 class Usuario(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    id_usuario = models.UUIDField(unique = True, default = uuid.uuid4, editable = False)
     nome = models.CharField(max_length = 255, blank = False)
     matricula = models.IntegerField(blank=False, null = False, unique = True)
     email = models.EmailField(max_length = 70, blank = False, null = False, unique = True )
@@ -21,3 +11,13 @@ class Usuario(models.Model):
     def __str__(self):
         return self.nome
 
+class Arquivo(models.Model):
+    id_arquivo = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+    descricao = models.CharField(max_length = 255, blank = False)
+    data = models.DateField(auto_now_add = True)
+    copias = models.IntegerField(blank = False, null = False)
+    nome = models.CharField(max_length = 70, blank = False)
+
+    def __str__(self):
+        return self.nome
